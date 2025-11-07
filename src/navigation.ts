749cdb8d90c818be3bc3e75c08b1b196c8461b95
lx521603 <32638642+lx521603@users.noreply.github.com>
2025-11-07 16:53:57 +0800

    更新 navigation.ts

--- src/navigation.ts
+++ src/navigation.ts
@@ -5,7 +5,7 @@ export const headerData = {
     { text: '主页', href: getPermalink('/'), links: [] },
     { text: '博客', href: getBlogPermalink(), links: [] },
     { text: '分类', href: getPermalink('tutorials','category'), },
-    { text: '标签', href: getPermalink('astro','tag'), },
+    { text: '标签', href: getPermalink('blog','tag'), },
     { text: '联系', href: getPermalink('/contact'), links: [] },
   ],
   actions: [], // 不需要按钮
