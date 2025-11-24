617a32ca342cf727c523ba71db4b9fae986570ac
lx521603 <32638642+lx521603@users.noreply.github.com>
2025-11-25 01:00:49 +0800

    更新 astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -86,6 +86,7 @@ export default defineConfig({
     resolve: {
       alias: {
         '~': path.resolve(__dirname, './src'),
+        '@/': path.resolve(__dirname, './src/'),
       },
     },
   },
